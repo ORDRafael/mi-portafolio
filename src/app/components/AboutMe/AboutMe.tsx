6c08fcc55978075db9269704a4dfd4ae3a745f9b
Rafael <rafaelordaz16@gmail.com>
2025-07-06 00:48:21 -0400

    Cambio en descripcion

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -5,7 +5,7 @@ const aboutMe = [
     {
     imagenUrl: "ajfniejgene;wr",
     title: "Hola, soy Rafael",
-    description: "Soy Rafael, un programador con experiencia en desarrollo de software. Me especializo en el desarrollo de aplicaciones web y móviles, y tengo un fuerte compromiso con la calidad y el rendimiento de mis proyectos. Manejo tecnologías como React, PHP, Flutter, n8n y más. Estoy disponible para ayudarte en tus proyectos. Desarrollo scripts, apps moviles y mantenimiento de las mismas. Me centro en la creacion de soluciones eficientes y escalables para las necesidades del cliente."
+    description: "Un programador con experiencia en desarrollo de software. Me especializo en el desarrollo de aplicaciones web y móviles, y tengo un fuerte compromiso con la calidad y el rendimiento de mis proyectos. Manejo tecnologías como React, PHP, Flutter, n8n y más. Estoy disponible para ayudarte en tus proyectos. Desarrollo scripts, apps moviles y mantenimiento de las mismas. Me centro en la creacion de soluciones eficientes y escalables para las necesidades del cliente."
 },
 ]
 
