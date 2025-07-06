5c2cee4a73dab1165830321daa5c089e2d49078d
Rafael <rafaelordaz16@gmail.com>
2025-07-06 00:11:41 -0400

    Quite el subtitle

--- src/app/components/Experiencia/Experiencia.tsx
+++ src/app/components/Experiencia/Experiencia.tsx
@@ -7,13 +7,11 @@ export default function Experiencia(){
         {
             timeline: "May 2024 - Feb 2025",
             title: "Besser Solutions",
-            subtitle: "Programador junior",
             description: "Como parte del equipo, fui responsable del desarrollo de scripts avanzados en Python y Bash para la optimización de flujos de trabajo. Lideré la creación de una aplicación móvil con Flutter, abarcando desde la fase de diseño UI/UX en Figma hasta su implementación. Mis responsabilidades también incluyeron la adaptación y optimización de código fuente de sistemas existentes, el mantenimiento y depuración de aplicaciones internas críticas, y la gestión de configuraciones de usuario en diversos sistemas operativos (Linux, Windows, macOS), asegurando la operatividad y eficiencia."
         },
         {
             timeline: "Sep 2023 - Jan 2024",
             title: "Hospital Calle Sierra",
-            subtitle: "Desarrollador",
             description: "Lideré el desarrollo de un sistema de gestión de usuarios, doctores y citas con recordatorios para un hospital. Durante 6 meses, trabajé directamente en las instalaciones para comprender a fondo los requerimientos y las necesidades operativas del equipo de ingeniería de turno. Este proyecto mejoró la eficiencia en la administración de pacientes y la comunicación interna."
         },
     ];
